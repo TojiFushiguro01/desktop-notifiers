5c301fb827ecaf830c10ab835c5ba8a61cbac172
Harsh Verma <164463743+TojiFushiguro01@users.noreply.github.com>
2024-07-30 10:50:02 +0530

    Add files via upload

---
+++ Lunch Time Notifier/Lunch Time.py
@@ -0,0 +1,26 @@
+import datetime
+import time
+from plyer import notification
+
+def notify_lunch():
+
+    notification.notify(
+        title="Lunch Time {}".format(time.strftime("%H:%M:%S",time.localtime())),
+        message="Its Time to get something to Eat.", 
+        app_name="Daily Routine Schedular", 
+        app_icon="icon.png",
+        timeout=10,
+        toast=False
+    )
+
+while True:
+
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M")
+
+    if current_time == "12:00":
+
+        notify_lunch()
+        break
+
+      
